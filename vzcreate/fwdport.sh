f95e8c4cace79bdeee7846a60824a6883da5e698
Roman Ovchinnikov <coolthecold@gmail.com>
2012-08-14 13:29:49 +0400

    make fwdport.sh use config file

--- vzcreate/fwdport.sh
+++ vzcreate/fwdport.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+vzconfig=vzcreate.conf
+
 ARGS=3
 if [[ $# -ne "$ARGS"  ]]
 then
@@ -7,6 +9,15 @@ then
     exit 1
 
 fi
-	    
+
+if [ -r $vzconfig ]; then
+    . $vzconfig
+else
+    echo "failed to read config $vzconfig"
+    exit 2
+fi
+
+if [ -z "$myip" ];then echo "myip is empty!";exit 2;fi
+if [ -z "$GINIF" ];then echo "GINIF is empty!";exit 2;fi
 #enabling forwarding for nginx
-iptables -t nat -A PREROUTING -p tcp -d 87.118.90.42 --dport $2 -i eth0 -j DNAT --to-destination $1:$3
+iptables -t nat -A PREROUTING -p tcp -d "${myip}" --dport $2 -i "${GINIF}" -j DNAT --to-destination $1:$3
